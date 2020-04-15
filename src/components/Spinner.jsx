395737c922a1d503659320265fd716a70cd8cd8a
Michael <osoba.michael@outlook.com>
2020-04-15 23:47:58 +0100

    Created spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Spinner() {
+    return (
+        <div className="m-auto text-center">
+            <img src="loader.gif" className="w-50" alt="loading..."/>
+        </div>
+    )
+}
