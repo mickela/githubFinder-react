c047dc08dfa9a6e6bb41d9d366a3ce12b5a24c52
Michael <osoba.michael@outlook.com>
2020-04-15 04:08:08 +0100

    created navbar component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Nav() {
+    return (
+        <div>
+            <nav className=" navbar navbar-expand-lg navbar-danger bg-danger ">
+                <a className="navbar-brand text-light" href="#_">
+                    <img src="logo.png" alt="logo"/>
+                    <strong>GitHub Finder</strong>
+                </a>
+            </nav>
+        </div>
+    )
+}
